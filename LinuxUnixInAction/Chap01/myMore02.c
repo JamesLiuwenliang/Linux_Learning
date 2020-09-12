dd729d190f0f853d31cbd75fd4fb53c3dde862bf
JamesLiuwenliang <13114817178@163.com>
2020-09-12 23:39:17 +0900

    add

--- LinuxUnixInAction/Chap01/myMore02.c
+++ LinuxUnixInAction/Chap01/myMore02.c
@@ -4,6 +4,8 @@
 #define LINELEN 512
 /** 
  * 优化方案：从标准输入中读入要分页的数据，直接从键盘读用户的输入 
+ * 有一个文件 /dev/tty 这是键盘和显示器的设备描述文件，向这个文件 写 相当于显示在用户的屏幕上， 读 相当于从键盘获取用户的输入
+ * `ls /bin | ./more02` : more有两个输入 ：1.程序的标准输入是ls的输出 2. 用户的输入，从/dev/tty得到数据
  */
 int see_more(FILE *cmd){
     int c ;
@@ -31,7 +33,11 @@ void do_more(FILE *fp){
     char line[LINELEN];
     int num_of_lines = 0;
     int see_more(FILE *) ,reply;
+
+    /* 对/dev/tty 读用户输入 */
     FILE *fp_tty;
+
+    /*CMD stream*/
     fp_tty = fopen("/dev/tty","r");
 
     if(fp_tty == NULL){
@@ -48,6 +54,7 @@ void do_more(FILE *fp){
             num_of_lines -= reply;
         }
 
+        /* 显示一行 */
         if(fputs(line,stdout) == EOF){
             exit(1);
         }
